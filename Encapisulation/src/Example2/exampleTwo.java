f3e865364b8a07ad0484e053aad375338ed92fc6
Paulo9014 <paulofrf40@gmail.com>
2025-07-28 10:58:32 -0300

    fiz um terceiro exemplo de get e set, com a utilização de construtores, media de notas.

---
+++ Encapisulation/src/Example2/exampleTwo.java
@@ -0,0 +1,13 @@
+package Example2;
+
+public class exampleTwo {
+    private int idade;
+    
+    public int getIdade(){
+        return idade;
+    }
+
+    public void setIdade(int newAge){
+        this.idade = newAge;
+    }
+}
